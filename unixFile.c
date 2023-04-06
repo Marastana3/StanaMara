c7abe8b0a24d4b7941778f9d5d7bda7fccf5dad4
Marastana3 <smaramaria@ymail.com>
2023-04-06 10:22:23 +0300

    added menu for regular file (not perfect yet)

--- unixFile.c
+++ unixFile.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <sys/stat.h>
 
+#define MAX_LENGTH 1
+
 void print_file_info(const char *path) {
     struct stat st;
     char type;
@@ -16,12 +18,62 @@ void print_file_info(const char *path) {
         type = 'R';
     } else if (S_ISLNK(st.st_mode)) {
         type = 'L';
+    } else if (S_ISDIR(st.st_mode)) {
+        type = 'D';
     } else {
         type = '-';
     }
     
     printf("%s - %s\n", path, (type == 'R') ? "REGULAR FILE" :
-           ((type == 'L') ? "SYMBOLIC LINK" : "OTHER"));
+           ((type == 'L') ? "SYMBOLIC LINK" : (type == 'D') ? 
+           "DIRECTORY" : "OTHER"));
+}
+
+void print_menu_regular_file(char* argv[]){
+    char option[MAX_LENGTH];
+    printf("~~~~~~~~~~ MENU ~~~~~~~~~~\n");
+    
+     do {
+        printf("\nPlease select an option:\n");
+        printf("-n NAME\n");
+        printf("-d SIZE\n");
+        printf("-h HARD DISK COUNT\n");
+        printf("-m TIME OF LAST MODIFICATION\n");
+        printf("-a ACCESS RIGHTS\n");
+        printf("-l CREATE SYMBOLIC LINK\n");
+        printf("-q Quit\n");
+
+        for(int i = 0; i < MAX_LENGTH; ++i){
+            scanf("%c", &option[i]);
+        }
+
+        switch(option[1]) {
+            case 'n':
+                printf("You selected NAME .\n");
+                break;
+            case 'd':
+                printf("You selected SIZE.\n");
+                break;
+            case 'h':
+                printf("You selected HARD DISK COUNT\n");
+                break;
+            case 'm':
+                printf("You selected TIME OF LAST MODIFICATION .\n");
+                break;
+            case 'a':
+                printf("You selected ACCESS RIGHTS .\n");
+                break;
+            case 'l':
+                printf("You selected CREATE SYMBOLIC LINK .\n");
+                break;
+            case 'q':
+                printf("Goodbye!\n");
+                break;
+            default:
+                printf("Invalid option. Please try again.\n");
+        }
+    } while (option != 'q');
+
 }
 
 int main(int argc, char* argv[]) {
@@ -30,6 +82,7 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
     print_file_info(argv[1]);
+    print_menu_regular_file(argv[1]);
     return 0;
 }
 //check for input string, if letter not valid option --> error message + re-print menu
